0ad8d75569640313aca73be50e10c19685728a11
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-06-11 00:45:46 +0300

    Refactor Category page

---
+++ app/(site)/[type]/interface.ts
@@ -0,0 +1,5 @@
+export interface CategoryProps {
+  params: {
+    type: string;
+  };
+}
--- app/(site)/[type]/page.tsx
+++ app/(site)/[type]/page.tsx
@@ -1,5 +1,36 @@
-const Courses = () => {
-  return <div>Courses</div>;
+import { notFound } from 'next/navigation';
+import type { CategoryProps } from './interface';
+import { CourseContextProvider } from '@/app/context';
+import { getPage } from '@/api';
+import { ProductList } from '@/components';
+import classes from './page.module.css';
+
+const Category = async ({ params }: CategoryProps) => {
+  const { type } = await params;
+
+  let targetCategory = '';
+  if (type === 'courses') targetCategory = 'Разработка';
+  if (type === 'books') targetCategory = 'Книги';
+
+  if (!targetCategory) notFound();
+
+  const pages = await getPage(undefined, targetCategory);
+
+  if (!pages) {
+    notFound();
+  }
+
+  const allProducts = pages.flatMap((page) => page.products);
+
+  return (
+    <CourseContextProvider pageId={type} initialProducts={allProducts}>
+      <div className={classes.container}>
+        <h1>{type === 'courses' ? 'Курсы по разработке' : 'Книги'}</h1>
+
+        <ProductList products={allProducts} />
+      </div>
+    </CourseContextProvider>
+  );
 };
 
-export default Courses;
+export default Category;
